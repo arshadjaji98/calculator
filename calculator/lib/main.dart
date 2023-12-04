77030d085ee0e80c07ec46f7ffe31c77350b02bc
arshadjaji98 <147608579+arshadjaji98@users.noreply.github.com>
2023-12-04 14:42:07 +0500

    update

---
+++ calculator/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:calculator/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
